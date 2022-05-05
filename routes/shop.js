ba3e3de8656639c074df1179611d2c77b004f8f6
sofiacrf <sofia.rojasflo@gmail.com>
2022-05-05 18:38:22 -0500

    added missing routes

--- routes/shop.js
+++ routes/shop.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
@@ -10,12 +11,18 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-router.get('products/:.productId', shopController.getProduct);
+router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart', shopController.getCart);
+router.get('/cart', isAuth, shopController.getCart);
 
-router.get('/orders', shopController.getOrders);
+router.post('/cart', isAuth, shopController.postCart);
 
-router.get('/checkout', shopController.getCheckout);
+router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
+
+router.get('/checkout', isAuth, shopController.getCheckout);
+
+router.get('/orders', isAuth, shopController.getOrders);
+
+router.get('/orders/:orderId', isAuth, shopController.getInvoice);
 
 module.exports = router;
